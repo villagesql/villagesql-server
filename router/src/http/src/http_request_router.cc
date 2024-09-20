mysql/mysql-server

Bug#36797250 http-request-router is slow to match URLs
Benchmarking the HTTP request handling showed that quite some time is
spent in matching the URI-path for the request router.

`perf` shows that the time is spent in std::regex_match().

Change
======

- added benchmarks for std::regex and ICU regex
- use ICU's regex implementation instead of std::regex

Benchmark:

std::regex    330 ns/iter
icu->find()   173 ns/iter

Change-Id: I9dd939f9477da594538b05a258e0cec1f192959f

router/src/http/src/http_request_router.cc
@@ -25,10 +25,18 @@
 
 #include "http_request_router.h"
 
-#include <regex>
+#include <unicode/regex.h>
+#include <unicode/unistr.h>
+#include <unicode/utypes.h>
+
+#include <algorithm>
+#include <memory>
+#include <stdexcept>
 #include <string>
+#include <string_view>
 #include <utility>
 
+#include "mysql/harness/stdx/expected.h"
 #include "mysqlrouter/component/http_auth_realm_component.h"
 #include "mysqlrouter/component/http_server_auth.h"
 
@@ -38,6 +46,35 @@ IMPORT_LOG_FUNCTIONS()
 
 using BaseRequestHandlerPtr = HttpRequestRouter::BaseRequestHandlerPtr;
 
+stdx::expected<void, UErrorCode> HttpRequestRouter::RouteMatcher::compile() {
+  UErrorCode out_status = U_ZERO_ERROR;
+
+  std::unique_ptr<icu::RegexPattern> pattern(
+      icu::RegexPattern::compile(url_pattern_.c_str(), 0, out_status));
+  if (out_status != U_ZERO_ERROR) return stdx::unexpected(out_status);
+
+  regex_pattern_ = std::move(pattern);
+
+  return {};
+}
+
+stdx::expected<void, UErrorCode> HttpRequestRouter::RouteMatcher::matches(
+    std::string_view input) const {
+  return matches(icu::UnicodeString(input.data(), input.size()));
+}
+
+stdx::expected<void, UErrorCode> HttpRequestRouter::RouteMatcher::matches(
+    const icu::UnicodeString &input) const {
+  UErrorCode out_status = U_ZERO_ERROR;
+
+  std::unique_ptr<icu::RegexMatcher> regex_matcher(
+      regex_pattern_->matcher(input, out_status));
+
+  const auto find_res = regex_matcher->find(out_status);
+  if (find_res == 0) return stdx::unexpected(out_status);
+
+  return {};
+}
 /**
  * Request router
  *
@@ -48,19 +85,26 @@ using BaseRequestHandlerPtr = HttpRequestRouter::BaseRequestHandlerPtr;
 void HttpRequestRouter::append(const std::string &url_regex_str,
                                std::unique_ptr<http::base::RequestHandler> cb) {
   log_debug("adding route for regex: %s", url_regex_str.c_str());
-  std::unique_lock lock(route_mtx_);
 
-  request_handlers_.emplace_back(RouterData{
-      url_regex_str, std::regex{url_regex_str, std::regex_constants::extended},
-      std::move(cb)});
+  RouteMatcher matcher(url_regex_str, std::move(cb));
+
+  auto compile_res = matcher.compile();
+  if (!compile_res) {
+    throw std::runtime_error("compile of " + url_regex_str +
+                             "failed with status " +
+                             std::to_string(compile_res.error()));
+  }
+
+  std::unique_lock lock(route_mtx_);
+  request_handlers_.push_back(std::move(matcher));
 }
 
 void HttpRequestRouter::remove(const void *handler_id) {
   std::unique_lock lock(route_mtx_);
 
   for (auto it = request_handlers_.begin(); it != request_handlers_.end();) {
-    if (it->handler.get() == handler_id) {
-      log_debug("removing route for regex: %s", it->url_regex_str.c_str());
+    if (it->handler().get() == handler_id) {
+      log_debug("removing route for regex: %s", it->url_pattern().c_str());
       it = request_handlers_.erase(it);
     } else {
       ++it;
@@ -73,7 +117,7 @@ void HttpRequestRouter::remove(const std::string &url_regex_str) {
   std::unique_lock lock(route_mtx_);
 
   for (auto it = request_handlers_.begin(); it != request_handlers_.end();) {
-    if (it->url_regex_str == url_regex_str) {
+    if (it->url_pattern() == url_regex_str) {
       it = request_handlers_.erase(it);
     } else {
       it++;
@@ -150,12 +194,18 @@ void HttpRequestRouter::route(http::base::Request &req) {
 }
 
 BaseRequestHandlerPtr HttpRequestRouter::find_route_handler(
-    const std::string &path) {
+    std::string_view path) {
+  // as .matches() is called in a loop on the same string,
+  // convert it to UnicodeString upfront.
+  //
+  // That saves doing the same conversion for each path under a lock.
+  icu::UnicodeString uni_path(path.data(), path.size());
+
   std::shared_lock lock(route_mtx_);
 
   for (auto &request_handler : request_handlers_) {
-    if (std::regex_search(path, request_handler.url_regex)) {
-      return request_handler.handler;
+    if (request_handler.matches(uni_path)) {
+      return request_handler.handler();
     }
   }
 

villagesql/villagesql-server

router/src/http/src/http_request_router.cc
@@ -25,10 +25,18 @@
 
 #include "http_request_router.h"
 
-#include <regex>
+#include <unicode/regex.h>
+#include <unicode/unistr.h>
+#include <unicode/utypes.h>
+
+#include <algorithm>
+#include <memory>
+#include <stdexcept>
 #include <string>
+#include <string_view>
 #include <utility>
 
+#include "mysql/harness/stdx/expected.h"
 #include "mysqlrouter/component/http_auth_realm_component.h"
 #include "mysqlrouter/component/http_server_auth.h"
 
@@ -37,6 +45,36 @@
 IMPORT_LOG_FUNCTIONS()
 
 using BaseRequestHandlerPtr = HttpRequestRouter::BaseRequestHandlerPtr;
+
+stdx::expected<void, UErrorCode> HttpRequestRouter::RouteMatcher::compile() {
+  UErrorCode out_status = U_ZERO_ERROR;
+
+  std::unique_ptr<icu::RegexPattern> pattern(
+      icu::RegexPattern::compile(url_pattern_.c_str(), 0, out_status));
+  if (out_status != U_ZERO_ERROR) return stdx::unexpected(out_status);
+
+  regex_pattern_ = std::move(pattern);
+
+  return {};
+}
+
+stdx::expected<void, UErrorCode> HttpRequestRouter::RouteMatcher::matches(
+    std::string_view input) const {
+  return matches(icu::UnicodeString(input.data(), input.size()));
+}
+
+stdx::expected<void, UErrorCode> HttpRequestRouter::RouteMatcher::matches(
+    const icu::UnicodeString &input) const {
+  UErrorCode out_status = U_ZERO_ERROR;
+
+  std::unique_ptr<icu::RegexMatcher> regex_matcher(
+      regex_pattern_->matcher(input, out_status));
+
+  const auto find_res = regex_matcher->find(out_status);
+  if (find_res == 0) return stdx::unexpected(out_status);
+
+  return {};
+}
 
 /**
  * Request router
@@ -50,28 +88,50 @@ void HttpRequestRouter::append(const std::string &url_host,
                                std::unique_ptr<http::base::RequestHandler> cb) {
   log_debug("adding route for regex: %s, url_host: '%s'", url_regex_str.c_str(),
             url_host.c_str());
-  std::lock_guard<std::mutex> lock(route_mtx_);
-  auto &request_handlers =
-      url_host.empty() ? request_handlers_url_host_empty_ : request_handlers_;
-  request_handlers.emplace_back(
-      RouterData{url_host, url_regex_str,
-                 std::regex{url_regex_str, std::regex_constants::extended},
-                 std::move(cb)});
+
+  RouteMatcher matcher(url_regex_str, std::move(cb));
+
+  auto compile_res = matcher.compile();
+  if (!compile_res) {
+    throw std::runtime_error("compile of " + url_regex_str +
+                             "failed with status " +
+                             std::to_string(compile_res.error()));
+  }
+
+  std::unique_lock lock(route_mtx_);
+  auto req_it = request_handlers_.find(url_host);
+  if (req_it == request_handlers_.end()) {
+    std::vector<RouteMatcher> router_matchers;
+    router_matchers.emplace_back(std::move(matcher));
+
+    request_handlers_.emplace(url_host, std::move(router_matchers));
+  } else {
+    req_it->second.emplace_back(std::move(matcher));
+  }
 }
 
 void HttpRequestRouter::remove(const void *handler_id) {
-  std::lock_guard<std::mutex> lock(route_mtx_);
-  for (auto &request_handlers : {std::ref(request_handlers_),
-                                 std::ref(request_handlers_url_host_empty_)}) {
-    for (auto it = request_handlers.get().begin();
-         it != request_handlers.get().end();) {
-      if (it->handler.get() == handler_id) {
+  std::unique_lock lock(route_mtx_);
+
+  for (auto map_it = request_handlers_.begin();
+       map_it != request_handlers_.end();) {
+    auto &[url_host, request_handlers] = *map_it;
+    for (auto it = request_handlers.begin(); it != request_handlers.end();) {
+      if (it->handler().get() == handler_id) {
         log_debug("removing route for regex: %s, url_host: '%s'",
-                  it->url_regex_str.c_str(), it->url_host.c_str());
-        it = request_handlers.get().erase(it);
+                  it->url_pattern().c_str(), url_host.c_str());
+        it = request_handlers.erase(it);
       } else {
         ++it;
       }
+    }
+
+    if (request_handlers.empty()) {
+      // if there are no more request-handlers for a hostname, remove the entry
+      // in the hostname map.
+      map_it = request_handlers_.erase(map_it);
+    } else {
+      ++map_it;
     }
   }
 }
@@ -80,15 +140,26 @@ void HttpRequestRouter::remove(const std::string &url_host,
                                const std::string &url_regex_str) {
   log_debug("removing route for regex: %s, url_host: '%s'",
             url_regex_str.c_str(), url_host.c_str());
-  std::lock_guard<std::mutex> lock(route_mtx_);
-  auto &request_handlers =
-      url_host.empty() ? request_handlers_url_host_empty_ : request_handlers_;
+
+  std::unique_lock lock(route_mtx_);
+
+  const auto req_it = request_handlers_.find(url_host);
+  if (req_it == request_handlers_.end()) return;
+
+  auto &request_handlers = req_it->second;
+
   for (auto it = request_handlers.begin(); it != request_handlers.end();) {
-    if (it->url_host == url_host && it->url_regex_str == url_regex_str) {
+    if (it->url_pattern() == url_host && it->url_pattern() == url_regex_str) {
       it = request_handlers.erase(it);
     } else {
       it++;
     }
+  }
+
+  if (request_handlers.empty()) {
+    // if there are no more request-handlers for a hostname, remove the entry in
+    // the hostname map.
+    request_handlers_.erase(req_it);
   }
 }
 
@@ -111,13 +182,13 @@ void HttpRequestRouter::handler_not_found(http::base::Request &req) {
 void HttpRequestRouter::set_default_route(
     std::unique_ptr<http::base::RequestHandler> cb) {
   log_debug("adding default route");
-  std::lock_guard<std::mutex> lock(route_mtx_);
+  std::unique_lock lock(route_mtx_);
   default_route_ = std::move(cb);
 }
 
 void HttpRequestRouter::clear_default_route() {
   log_debug("removing default route");
-  std::lock_guard<std::mutex> lock(route_mtx_);
+  std::unique_lock lock(route_mtx_);
   default_route_ = nullptr;
 }
 
@@ -165,22 +236,37 @@ void HttpRequestRouter::route(http::base::Request &req) {
 }
 
 BaseRequestHandlerPtr HttpRequestRouter::find_route_handler(
-    const std::string &url_host, const std::string &path) {
-  std::lock_guard<std::mutex> lock(route_mtx_);
+    std::string_view url_host, std::string_view path) {
+  // as .matches() is called in a loop on the same string,
+  // convert it to UnicodeString upfront.
+  //
+  // That saves doing the same conversion for each path under a lock.
+  icu::UnicodeString uni_path(path.data(), path.size());
 
-  for (auto &request_handler : request_handlers_) {
-    // TODO(areliga): should we do simple == here or some regex matching is
-    // needed?
-    if (url_host == request_handler.url_host &&
-        std::regex_search(path, request_handler.url_regex)) {
-      return request_handler.handler;
+  std::shared_lock lock(route_mtx_);
+
+  if (!url_host.empty()) {
+    auto req_it = request_handlers_.find(url_host);
+    if (req_it != request_handlers_.end()) {
+      auto &request_handlers = req_it->second;
+
+      for (auto &request_handler : request_handlers) {
+        if (request_handler.matches(uni_path)) {
+          return request_handler.handler();
+        }
+      }
     }
   }
 
-  // no hanlder with matching host found, try the one with empty host
-  for (auto &request_handler : request_handlers_url_host_empty_) {
-    if (std::regex_search(path, request_handler.url_regex)) {
-      return request_handler.handler;
+  // no handler with matching host found, try the one with empty host
+  auto req_it = request_handlers_.find("");
+  if (req_it != request_handlers_.end()) {
+    auto &request_handlers = req_it->second;
+
+    for (auto &request_handler : request_handlers) {
+      if (request_handler.matches(uni_path)) {
+        return request_handler.handler();
+      }
     }
   }
 