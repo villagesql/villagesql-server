0a41e4df4b2f8ff64bb1d324a5d1d33a7d6dc258
Tor Didriksen <tor.didriksen@oracle.com>
2025-01-14 15:47:44 +0100

    Bug#37387318 Add standalone xxhash library
    We have various implementations of xxhash already, bundled with other
    libraries.  The server code currently uses the version that comes with
    the bundled lz4 library.
    
    Download xxHash-0.8.3.tar.gz from https://github.com/Cyan4973/xxHash
    and used that one instead.
    
    We only add the files:
      xxh_x86dispatch.c  xxh_x86dispatch.h  xxhash.c  xxhash.h
    
    Change-Id: If5538b26177e148394adb699935d682385f65f5f
    (cherry picked from commit f429af7dc664c771f4fc4f4a85929882ca1618b4)
    (cherry picked from commit 84ddd2c49e24538eae45bacf04a0dbd03b2c5460)

--- sql/rpl_write_set_handler.cc
+++ sql/rpl_write_set_handler.cc
@@ -32,7 +32,7 @@
 #include <utility>
 #include <vector>
 
-#include "extra/lz4/my_xxhash.h"  // IWYU pragma: keep
+#include "extra/xxhash/my_xxhash.h"  // IWYU pragma: keep
 #include "lex_string.h"
 #include "m_ctype.h"
 #include "my_base.h"
