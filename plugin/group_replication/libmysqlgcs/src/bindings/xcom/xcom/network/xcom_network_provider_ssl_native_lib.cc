d8aed8b8de1c2756e0d8e2c68c229df805c9f666
Nuno Carvalho <nuno.carvalho@oracle.com>
2025-02-05 20:01:45 +0100

    BUG#37475769: fatal error: 'openssl/engine.h' file not found
    The OpenSSL Engine interface is deprecated, some Linux distros are
    already not including it on OpenSSL v3 main packages.
    
    To avoid build issues, the usage of OpenSSL Engine interface on GCS
    is restricted to OpenSSL version lower than 1.1.
    
    Change-Id: Ib9cedefc67f9312e92f7f7f7cee902cc4c5b3d99

--- plugin/group_replication/libmysqlgcs/src/bindings/xcom/xcom/network/xcom_network_provider_ssl_native_lib.cc
+++ plugin/group_replication/libmysqlgcs/src/bindings/xcom/xcom/network/xcom_network_provider_ssl_native_lib.cc
@@ -41,7 +41,9 @@
 #include "my_compiler.h"
 #endif
 
-#include "openssl/engine.h"
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#include <openssl/engine.h>
+#endif /* OPENSSL_VERSION_NUMBER < 0x10100000L */
 
 #include "xcom/task_debug.h"
 #include "xcom/x_platform.h"
@@ -564,7 +566,9 @@ void Xcom_network_provider_ssl_library::xcom_destroy_ssl() {
   }
 
 #if defined(WITH_SSL_STANDALONE)
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
   ENGINE_cleanup();
+#endif /* OPENSSL_VERSION_NUMBER < 0x10100000L */
   EVP_cleanup();
   CRYPTO_cleanup_all_ex_data();
   ERR_free_strings();
