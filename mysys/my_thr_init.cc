f943cd4fa8595a48ba76bda66f79577e2ca1e43d
John David Duncan <john.duncan@oracle.com>
2025-02-21 11:09:13 -0800

    Bug#36383937 post-push fix
    Fix for syntax error after merge.
    
    Change-Id: I9cf9c5a5e9ae4be40ebe7e2005187cf61cd94286

--- mysys/my_thr_init.cc
+++ mysys/my_thr_init.cc
@@ -316,7 +316,6 @@ bool my_thread_is_inited() {
 
 void my_thread_end() {
 #ifdef HAVE_PSI_THREAD_INTERFACE
-#ifdef HAVE_PSI_THREAD_INTERFACE
   /*
     Remove the instrumentation for this thread.
     This must be done before trashing st_my_thread_var,
