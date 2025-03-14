03347ddd1492d71e800d226405b5aa37ebbafe38
Tor Didriksen <tor.didriksen@oracle.com>
2025-03-14 15:21:12 +0100

    Bug#37709169 Bug in list handling in older cmake versions
    With un-quoted arguments here
          TARGET_LINK_OPTIONS(${target} PRIVATE
            -Wno-error=alloc-size-larger-than=
            -Wno-error=stringop-overflow
            -Wno-error=stringop-overread
          )
    cmake 3.17 produced only -Wno-alloc-size-larger-than
    rather than
    -Wno-error=alloc-size-larger-than= -Wno-error=stringop-overflow -Wno-error=stringop-overread
    
    Change-Id: Iacef24cf68604fcaeada63f9ad53613c3f2f52b1

--- cmake/fprofile.cmake
+++ cmake/fprofile.cmake
@@ -187,9 +187,9 @@ MACRO(DOWNGRADE_STRINGOP_WARNINGS target)
   IF(MY_COMPILER_IS_GNU AND WITH_LTO AND FPROFILE_USE)
     IF(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 11)
       TARGET_LINK_OPTIONS(${target} PRIVATE
-        -Wno-error=alloc-size-larger-than=
-        -Wno-error=stringop-overflow
-        -Wno-error=stringop-overread
+        "-Wno-error=alloc-size-larger-than="
+        "-Wno-error=stringop-overflow"
+        "-Wno-error=stringop-overread"
       )
     ELSE()
       TARGET_LINK_OPTIONS(${target} PRIVATE
