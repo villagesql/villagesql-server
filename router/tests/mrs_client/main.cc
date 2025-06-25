01959854985111a4ac5ad14e05262be68c772db8
Andrzej Religa <andrzej.religa@oracle.com>
2025-06-25 08:10:55 +0200

    Bug#38114704 Router fails to compile on MSVC 19.44.3520
    The following error is seen:
    error C2220: the following warning is treated as an error [C:\build\sb_1-20123200-1750808179.2897456\release\router\tests\mrs_client\mysqlrouter_mrs_client.vcxproj]
    
    This patch adds missing throw for the exception that was meant to be
    thrown.
    
    Change-Id: I7a1fb1067bb49b1f42de939dc7843f2178412556

--- router/tests/mrs_client/main.cc
+++ router/tests/mrs_client/main.cc
@@ -441,7 +441,7 @@ std::vector<CmdOption> g_options{
      [](const std::string &value) {
        std::ifstream file{value};
        if (!file.is_open()) {
-         std::runtime_error("File with `payload`, cannot be opened.");
+         throw std::runtime_error("File with `payload`, cannot be opened.");
        }
 
        g_configuration.payload =
