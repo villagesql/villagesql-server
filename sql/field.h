634846ddd8237e86ff43b6f5995e32509c43ca23
Mike Maloney <maloney@villagesql.com>
2026-03-03 14:23:53 -0500

    Field, Item, what's the difference anyhow?

--- sql/field.h
+++ sql/field.h
@@ -1889,7 +1889,7 @@ class Field {
   const uchar *unpack_int64(uchar *to, const uchar *from) const;
 
  private:
-  // TODO(villagesql): Collapse these into one object (here and in Field)
+  // TODO(villagesql): Collapse these into one object (here and in Item)
   const villagesql::TypeContext *custom_type{nullptr};
   villagesql::TypeEncoder *type_encoder_{nullptr};
 
