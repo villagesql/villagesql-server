2f98e1e129513a9932ab7cebb00c2d67e53da2c8
Tomas Ulin <tomas@villagesql.com>
2026-03-19 13:22:28 +0100

    clarifying comment

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -119,7 +119,8 @@
 #include "string_with_len.h"
 #include "template_utils.h"
 #include "thr_mutex.h"
-// VillageSQL: Needed for destructor.
+// VillageSQL: Complete type needed for THD destructor to destroy
+// unique_ptr<TmpMetadata> member villagesql_tmp_metadata.
 #include "villagesql/schema/tmp_metadata.h"
 
 class Parse_tree_root;
