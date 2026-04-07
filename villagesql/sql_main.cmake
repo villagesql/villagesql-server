5c4c6d6bec6e3866072853fdb466a04e610dfeab
Tomas Ulin <tomas@villagesql.com>
2026-04-07 13:32:18 +0200

    minimize cmake diff

---
+++ villagesql/sql_main.cmake
@@ -0,0 +1,31 @@
+# Copyright (c) 2026 VillageSQL Contributors
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <https://www.gnu.org/licenses/>.
+
+# Links VillageSQL object files into sql_main.
+# Included from sql/CMakeLists.txt after ADD_STATIC_LIBRARY(sql_main ...).
+TARGET_SOURCES(sql_main PRIVATE
+  $<TARGET_OBJECTS:villagesql_common>
+  $<TARGET_OBJECTS:villagesql_schema>
+  $<TARGET_OBJECTS:villagesql_sql>
+  $<TARGET_OBJECTS:villagesql_system_views>
+  $<TARGET_OBJECTS:villagesql_types>
+  $<TARGET_OBJECTS:villagesql_vdf>
+)
+ADD_DEPENDENCIES(sql_main villagesql_common)
+ADD_DEPENDENCIES(sql_main villagesql_schema)
+ADD_DEPENDENCIES(sql_main villagesql_sql)
+ADD_DEPENDENCIES(sql_main villagesql_system_views)
+ADD_DEPENDENCIES(sql_main villagesql_types)
+ADD_DEPENDENCIES(sql_main villagesql_vdf)
---
+++ villagesql/subdirs.cmake
@@ -0,0 +1,25 @@
+# Copyright (c) 2026 VillageSQL Contributors
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <https://www.gnu.org/licenses/>.
+
+# VillageSQL subdirectories included from sql/CMakeLists.txt.
+# Binary dirs are relative to sql/'s build directory (CMAKE_CURRENT_BINARY_DIR).
+# Kept here so the listing lives alongside the villagesql source tree.
+ADD_SUBDIRECTORY(../villagesql/common ${CMAKE_CURRENT_BINARY_DIR}/villagesql/common)
+ADD_SUBDIRECTORY(../villagesql/schema ${CMAKE_CURRENT_BINARY_DIR}/villagesql/schema)
+ADD_SUBDIRECTORY(../villagesql/sql ${CMAKE_CURRENT_BINARY_DIR}/villagesql/sql)
+ADD_SUBDIRECTORY(../villagesql/system_views ${CMAKE_CURRENT_BINARY_DIR}/villagesql/system_views)
+ADD_SUBDIRECTORY(../villagesql/types ${CMAKE_CURRENT_BINARY_DIR}/villagesql/types)
+ADD_SUBDIRECTORY(../villagesql/vdf ${CMAKE_CURRENT_BINARY_DIR}/villagesql/vdf)
+ADD_SUBDIRECTORY(../villagesql/veb ${CMAKE_CURRENT_BINARY_DIR}/villagesql/veb)
