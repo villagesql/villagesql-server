12173fee44d118bec3dea22f8c13c8d411d21e51
Niklas Stangl <niklas@villagesql.com>
2026-07-06 16:48:02 +0200

    Simplify change to fix uninitialized pointer warning

--- sql/check_stack.cc
+++ sql/check_stack.cc
@@ -123,9 +123,9 @@ bool check_stack_overrun(const THD *thd, long margin, unsigned char *buf) {
   }
 #endif
 
-  char stack_addr{};
-  long stack_used =
-      used_stack(thd->thread_stack, reinterpret_cast<char *>(&stack_addr));
+  long stack_used{};
+  stack_used =
+      used_stack(thd->thread_stack, reinterpret_cast<char *>(&stack_used));
   if (stack_used >= static_cast<long>(my_thread_stack_size - margin) ||
       DBUG_EVALUATE_IF("simulate_stack_overrun", true, false)) {
     // Touch the buffer, so that it is not optimized away by -flto.
