08ff3d5a345c135803c6accfae3566c3293bc55d
Lee <leeca@villagesql.com>
2026-07-06 11:40:45 -0500

    Standardize build setup (#708)
    ## Description
    
    Standardizes build setup.
    
    ## Related Issue
    
    Part of the effort to Cleanup release-dev-server #595
    
    Created by bonsai from ad398d00b76ca97e96ddecd05f7bb902290eef32

--- scripts/setup_linux_build_env.sh
+++ scripts/setup_linux_build_env.sh
@@ -10,6 +10,7 @@ apt-get "${APT_OPTS[@]}" install -y --no-install-recommends \
          build-essential \
          cmake \
          libssl-dev \
+         libcurl4-openssl-dev \
          pkg-config \
          bison \
          libncurses5-dev \
