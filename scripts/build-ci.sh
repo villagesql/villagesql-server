b114ba1641df7dc136539c65e7e9bf13bcd1fffe
Lee <leeca@villagesql.com>
2026-07-30 08:50:00 -0500

    Retire script build-ci and test-ci (#888)
    ## Description
    
    Delete the original `scripts/build-ci.sh` and `scripts/test-ci.sh` now
    that the `villagesql/bld_tools/` versions are doing the job.
    
    ## Related Issue
    
    Part of the Cleanup release-dev-server #595 effort.
    
    Created by bonsai from d823a3e6a96184c83611466d16a530e78e7f902a

--- scripts/build-ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2026 VillageSQL Contributors
-# Configure and build the VillageSQL server for CI.
-
-# Delegate to the reference implementation in the villagesql/bld_tools directory
-SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-SOURCE_DIR="$(cd "$SCRIPTS_DIR/.." && pwd)"
-
-"$SOURCE_DIR/villagesql/bld_tools/build_ci.sh" "$@"
