cec9bb3096a175e00f51c28e3252663545992971
Avi <avivalse@villagesql.com>
2026-09-10 18:42:26 -0400

    Remove overloaded method

--- villagesql/include/byteorder.h
+++ villagesql/include/byteorder.h
@@ -25,17 +25,10 @@
 #include "my_inttypes.h"
 
 // Return a native-endian ulonglong from bytes pointed to by ptr.
-inline ulonglong ulonglongget(const uchar *ptr) {
+inline ulonglong ulonglongget(const char *ptr) {
   ulonglong val;
   memcpy(&val, ptr, sizeof(val));
   return val;
-}
-
-// Return a native-endian ulonglong from bytes pointed to by ptr. The bytes are
-// treated as unsigned.
-inline ulonglong ulonglongget(const char *ptr) {
-  return ulonglongget(
-      static_cast<const uchar *>(static_cast<const void *>(ptr)));
 }
 
 #endif  // VILLAGESQL_INCLUDE_BYTEORDER_H
