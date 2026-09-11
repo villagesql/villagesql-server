af324b988205823f9578e1ff2d4d51473ad441ba
Avi <avivalse@villagesql.com>
2026-09-11 15:04:17 -0400

    Add shellcheck fixes

--- villagesql/bld_tools/add_non_root_docker_user.sh
+++ villagesql/bld_tools/add_non_root_docker_user.sh
@@ -9,35 +9,35 @@
 # GROUP_NAME - group name for the group
 
 # Ubuntu images come prebuilt with user(ubuntu, 1000) and group(ubuntu, 1000). # Delete existing user(if any) on USER_ID.
-if getent passwd ${USER_ID} >/dev/null; then
-  userdel $(getent passwd ${USER_ID} | cut -d: -f1)
+if getent passwd "${USER_ID}" >/dev/null; then
+  userdel "$(getent passwd "${USER_ID}" | cut -d: -f1)"
 fi
 
 # Delete existing group(if any) on GROUP_ID
-if getent group ${GROUP_ID} >/dev/null; then
-  groupdel $(getent group ${GROUP_ID} | cut -d: -f1)
+if getent group "${GROUP_ID}" >/dev/null; then
+  groupdel "$(getent group "${GROUP_ID}" | cut -d: -f1)"
 fi
 
 # USER_NAME might already exist on a different uid other than USER_ID
 # Delete it if so.
-if getent passwd ${USER_NAME} >/dev/null; then
-  userdel ${USER_NAME}
+if getent passwd "${USER_NAME}" >/dev/null; then
+  userdel "${USER_NAME}"
 fi
 
 # GROUP_NAME might already exist on a different gid other than GROUP_ID
 # Delete it if so.
-if getent group ${GROUP_NAME} >/dev/null; then
-  groupdel ${GROUP_NAME}
+if getent group "${GROUP_NAME}" >/dev/null; then
+  groupdel "${GROUP_NAME}"
 fi
 
 # Add (GROUP_ID, GROUP_NAME)
-groupadd -g ${GROUP_ID} ${GROUP_NAME}
+groupadd -g "${GROUP_ID}" "${GROUP_NAME}"
 
 # Add (USER_ID, USER_NAME) within GROUP_ID
-useradd -l -u ${USER_ID} -g ${GROUP_ID} -m -s /bin/bash ${USER_NAME}
+useradd -l -u "${USER_ID}" -g "${GROUP_ID}" -m -s /bin/bash "${USER_NAME}"
 
 # Grant sudo privileges to USER_NAME
-usermod -a -G sudo ${USER_NAME}
+usermod -a -G sudo "${USER_NAME}"
 
 # Enable all sudoers to use sudo without password
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >>/etc/sudoers
